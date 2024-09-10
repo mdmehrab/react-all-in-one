54f195a176204efa85a4ff23db6e1f047da4af7b
rakib <mehrabishraqhossian@gmail.com>
2024-09-10 07:00:59 -0700

    added the sidebar manu

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import DynamicProduct from "./components/DynamicProduct";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidbar";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import { Route, Routes } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function App() {
   return (
     <>
       <Header />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productId" element={<DynamicProduct />} />
-      </Routes>
+      <div className="d-flex">
+        <Container fluid>
+          <Row>
+            <Col md={3}>
+              <Sidebar />
+            </Col>
+            <Col md={9}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/products" element={<Products />} />
+                <Route
+                  path="/products/:productId"
+                  element={<DynamicProduct />}
+                />
+              </Routes>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      3.20 Scaling Up with Reducer and Context Explain how to combine a reducer
+      with context, how to avoid passing state and dispatch through props and
+      how to keep context and state logic in a separate file Learn effective
+      techniques for using the Reducer and Context combo to scale up your React
+      applications in this video. Learn cutting-edge state management strategies
+      that will enable your components to effectively handle complicated data
+      flows. See how to get the codebase below, and check out other resources in
+      the reference section for a thorough grasp of scaling React applications.
+      How to run Please follow the below instructions to run this branch in your
+      machine: Login to the GitHub account on which you have been granted access
+      to this repository. If you have purchased the course but didn't get the
+      access to this repository, please contact our support team. You will find
+      contact details below.
+    </div>
+  );
 }
