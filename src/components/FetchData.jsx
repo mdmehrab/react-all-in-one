65127a4a836258f0c4578c9975693be3aab4ec83
rakib <mehrabishraqhossian@gmail.com>
2024-09-19 06:59:52 -0700

    trying to learn fetching data

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -5,51 +5,31 @@ import { Link } from "react-router-dom";
 function FetchData() {
   const [userInfo, setUserInfo] = useState([]);
 
-  useEffect(() => {
-    const getData = async () => {
-      const res = await axios.get(`https://jsonplaceholder.typicode.com/users`);
-      setUserInfo(res.data);
-    };
-
-    getData();
-  }, []);
-
   return (
     <>
       <section>
         <div className="container">
           <h2 className="text-center py-5">Fetch Data</h2>
           <div className="row">
-            {userInfo?.length > 0 ? (
-              userInfo?.map((user) => (
-                <>
-                  <div className="col-4 my-2">
-                    <div class="card">
-                      <div class="card-body">
-                        <h5 class="card-title">{user.name}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">
-                          {user.email}
-                        </h6>
-                        <p class="card-text">{user.address.street}</p>
-                        <p class="card-text">{user.address.suite}</p>
-                        <p class="card-text">{user.phone}</p>
-                        <p class="card-text">
-                          {user.company.bs || "any company"}
-                        </p>
-                        <Link to={`/details/${user.id}`} class="card-link">
-                          User Details
-                        </Link>
-                        <Link to="#" class="card-link text-uppercase">
-                          Album
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              ))
-            ) : (
-              <p className="text-center text-danger">No Data Found!</p>
-            )}
+            <div className="col-4 my-2">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title"></h5>
+                  <h6 className="card-subtitle mb-2 text-body-secondary">
+                    {user.username}
+                  </h6>
+                  <p className="card-text"></p>
+                  <p className="card-text"></p>
+                  <p className="card-text"></p>
+                  <Link to={`/details/`} className="card-link">
+                    User Details
+                  </Link>
+                  <Link to="#" className="card-link text-uppercase">
+                    Album
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,26 +1,4 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-
 export default function Details() {
-  const [data, setData] = useState({});
-  const { userDetails } = useParams();
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const mainData = await axios.get(
-          `https://jsonplaceholder.typicode.com/users/${userDetails}`
-        );
-        setData(mainData.data);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    getUserData();
-  }, [userDetails]);
-
   return (
     <>
       <section>
@@ -36,26 +14,16 @@ export default function Details() {
                   >
                     ID
                     <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                      {userDetails}
+                      12
                     </span>
                   </button>
                 </div>
 
-                <h5 className="card-title">
-                  {data?.name || "Name not available"}
-                </h5>
-                <h6 className="card-subtitle mb-2 text-danger">
-                  {data?.email || "Email not available"}
-                </h6>
-                <p className="card-text">
-                  {data?.address?.suite || "Address not available"}
-                </p>
-                <p className="card-text">
-                  {data?.phone || "Phone not available"}
-                </p>
-                <p className="card-text">
-                  {data?.company?.bs || "Company not available"}
-                </p>
+                <h5 className="card-title">dfgfd</h5>
+                <h6 className="card-subtitle mb-2 text-danger">dsfds</h6>
+                <p className="card-text">sdfds</p>
+                <p className="card-text">sdfds</p>
+                <p className="card-text">sdfdsf</p>
               </div>
             </div>
           </div>
