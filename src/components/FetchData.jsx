48ba66efc206e672af2c4a97fb613e141ababf04
rakib <mehrabishraqhossian@gmail.com>
2024-09-20 22:59:02 -0700

    fainaly learnd this

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 function FetchData() {
   const [userInfo, setUserInfo] = useState([]);
@@ -8,43 +7,29 @@ function FetchData() {
   useEffect(() => {
     const getData = async () => {
       const res = await axios.get(`https://jsonplaceholder.typicode.com/users`);
-
       setUserInfo(res.data);
     };
 
     getData();
   }, []);
+
   return (
     <>
       <section>
         <div className="container">
-          <h2 className="text-center py-5">Fetch Data</h2>
           <div className="row">
             {userInfo.map((user) => {
-              const {
-                id,
-                name,
-                email,
-                company: { name: companyName, bs },
-              } = user;
-
               return (
                 <>
-                  <div className="col-4 my-2" key={id}>
+                  <div className="col-4 py-5">
                     <div className="card">
-                      <div className="card-body">
-                        <h5 className="card-title">{name}</h5>
-                        <h6 className="card-subtitle mb-2 text-body-secondary"></h6>
-                        <p className="card-text">{email}</p>
-                        <p className="card-text">{companyName}</p>
-
-                        <Link to={`/details/${id}`} className="card-link">
-                          User Details
-                        </Link>
-                        <Link to="#" className="card-link text-uppercase">
-                          Album
-                        </Link>
-                      </div>
+                      <div className="card-header">Featured</div>
+                      <ul className="list-group list-group-flush">
+                        <li className="list-group-item">{user.id}</li>
+                        <li className="list-group-item">{user.name}</li>
+                        <li className="list-group-item">{user.username}</li>
+                        <li className="list-group-item">{user.email}</li>
+                      </ul>
                     </div>
                   </div>
                 </>
