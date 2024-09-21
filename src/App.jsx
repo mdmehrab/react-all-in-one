7f6123328ffa33f6eb35fbdc171f0ca1e9aec1e1
rakib <mehrabishraqhossian@gmail.com>
2024-09-21 17:37:46 +0600

    call api with img

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Details from "./pages/Details";
-import FetchData from "./components/FetchData";
+import { FetchData } from "./components/FetchData";
 
 export default function App() {
   return (
--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -1,45 +1,48 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-function FetchData() {
+export const FetchData = () => {
   const [userInfo, setUserInfo] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+      const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/photos`
+      );
       setUserInfo(res.data);
     };
 
     getData();
   }, []);
 
+  const renderedData = userInfo.slice(0, 10)?.map((item) => {
+    const { id, title, url, albumId, thumbnailUrl } = item;
+
+    return (
+      <div className="col-4" key={id}>
+        <div className="card">
+          <img src={thumbnailUrl} alt={title} height="300" />
+          <div className="card-body">
+            <h5 className="card-title">Album ID: {albumId}</h5>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">{title}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <>
       <section>
         <div className="container">
           <div className="row">
-            {userInfo.map((user) => {
-              return (
-                <>
-                  <div className="col-4 py-5">
-                    <div className="card">
-                      <div className="card-header">Featured</div>
-                      <ul className="list-group list-group-flush">
-                        <li className="list-group-item">{user.id}</li>
-                        <li className="list-group-item">{user.name}</li>
-                        <li className="list-group-item">{user.username}</li>
-                        <li className="list-group-item">{user.email}</li>
-                      </ul>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
+            {/* Render the mapped data */}
+            {renderedData}
           </div>
         </div>
       </section>
     </>
   );
-}
-
-export default FetchData;
+};
